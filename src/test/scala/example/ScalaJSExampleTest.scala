37a4b0f1c12fde91ec8235951b83db5d80d947bd
ed phillips <ed@cronos.net>
2016-05-15 12:42:55 -0700

    passing arbritrary test

--- src/test/scala/example/ScalaJSExampleTest.scala
+++ src/test/scala/example/ScalaJSExampleTest.scala
@@ -1,16 +1,30 @@
 package example
 
+import org.scalajs.dom
 
+import org.singlespaced.d3js.Ops._
+import org.singlespaced.d3js.d3
 import utest._
+
+import scala.scalajs.js
+
 
 object ScalaJSExampleTest extends TestSuite {
 
   import ScalaJSExample._
 
+
+  val elem=dom.document.createElement("p")
+  dom.document.body.appendChild(elem)
+  val sel=d3.selectAll("p").data(js.Array(5))
+
+
+
   def tests = TestSuite {
     'ScalaJSExample {
-      assert(svg)
+      sel.attr("tabindex", (d:Int) => d*2 )
 
+      assert("10" == elem.getAttribute("tabindex"))
     }
   }
 }
