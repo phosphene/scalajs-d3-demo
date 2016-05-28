dcd7934074979e2a3340d14f993faf1ecf0ef726
ed phillips <ed@cronos.net>
2016-05-28 13:02:56 -0700

    use JSConverter to convert to JSArray from Seq: passing test

--- src/main/scala/example/ScalaJSChart.scala
+++ src/main/scala/example/ScalaJSChart.scala
@@ -3,12 +3,17 @@ package example
 import org.singlespaced.d3js.Ops._
 import org.singlespaced.d3js.d3
 import scala.scalajs.js
+import js.JSConverters._
 
 object ScalaJSChart extends js.JSApp {
 
   def main(): Unit = {
-   val barChart = new BarChart
-   val sel = barChart.buildChart(js.Array(24,35,45,12))
+
+    val scSeq = Seq(24,35,45,12)
+    // Seq to js.Array -- Copy to js.Array
+    val jsArray: js.Array[Int] = scSeq.toJSArray
+    val barChart = new BarChart
+    val sel = barChart.buildChart(jsArray)
   }
 
 }
--- src/test/scala/example/BarChartTest.scala
+++ src/test/scala/example/BarChartTest.scala
@@ -3,14 +3,17 @@ package example
 import utest._
 import org.scalajs.dom
 import scala.scalajs.js
-
+import js.JSConverters._
 
 object BarChartTest extends TestSuite {
 
   def tests = TestSuite {
     'BarChart {
       val barChart = new BarChart
-      val sel = barChart.buildChart(js.Array(24,25,35,36))
+      val scSeq = Seq(24,25,35,36)
+      // Seq to js.Array -- Copy to js.Array
+      val jsArray: js.Array[Int] = scSeq.toJSArray
+      val sel = barChart.buildChart(jsArray)
       val actual = dom.document.querySelectorAll("rect")
       assert(actual.length == 4)
 
